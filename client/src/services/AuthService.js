dfed3a6d71054644db1bfd09320a5159e94c3fc5
thenuka99 <janithherath2@gmail.com>
2022-03-11 10:52:18 +0530

    Authservice folders #1

--- client/src/services/AuthService.js
+++ client/src/services/AuthService.js
@@ -3,8 +3,45 @@ import { baseUrl } from '../constants/constants';
 
 //start code here...
 //example
+
+//vault-folders
 export const NewFolder = (data) => {
-    return axios.post(baseUrl + '/vault', data, {
+    return axios.post(baseUrl + '/folders', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
+
+
+export const getFolders= (page) => {
+    return axios.get(baseUrl + '/folders?page=' + page, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
+
+
+export const addComputer = (data) => {
+    return axios.post(baseUrl + '/folders', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
+
+export const updateComputer = (id,data) => {
+    return axios.put(baseUrl + '/folders/', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
+
+
+export const deleteComputer = (id) => {
+    return axios.delete(baseUrl + '/folders/' + id, {
         headers: {
             'Content-Type': 'application/json'
         }
