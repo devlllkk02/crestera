528dd414376f04a95aa028c918e6302c5c2f20e0
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-12 23:55:00 +0530

    Created signup form header

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,12 +1,19 @@
 //------ Singup  ------
 import React from 'react'
+import Logo from "../../../public/images/logos/Crestera-Logo.png"
 
 function Signup() {
   return (
     <div className='signup'>
       <div className='signup__container'>
+        {/* Signup form begins here */}
         <form className='signup__form'>
-          
+          {/* Signup header begins here. It contains company logo and "SIGN UP" label*/}
+           <div className='signup__form__header'>
+
+             <img className='signup__form__logo' src={Logo} alt="Cresetra"/>
+             <h1>SIGN UP</h1>
+           </div>
         </form>
       </div>
     </div>
