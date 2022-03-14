e8bf2c63492bc6744e6fb12fe74dfd10703b7e35
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-14 14:51:39 +0530

    Styled signup page

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -9,31 +9,28 @@ function Signup() {
       <div className='signup__container'>
         {/* Signup form begins here */}
         <form className='signup__form'>
-          {/* Signup header begins here. It contains company logo and "SIGN UP" label*/}
-           <div className='signup__form__header'>
-
+             {/* Signup header */}
              <img className='signup__form__logo' src="./images/logos/Crestera-Logo.png" alt="Cresetra"/>
              <h1>SIGN UP</h1>
-           </div>
-            {/* Signup form content begins here */}
-            <div className='form-group'>
-                 <input type="text" className='form-control' placeholder='First Name'/>
+             {/* Signup content */}
+            <div className='signup__form__group'>
+                 <input type="text" className='signup__form__control' placeholder='First Name'/>
             </div>
-            <div className='form-group'>
-                 <input type="text" className='form-control' placeholder='Last Name'/>
+            <div className='signup__form__group'>
+                 <input type="text" className='signup__form__control' placeholder='Last Name'/>
             </div>
-            <div className='form-group'>
-                 <input type="text" className='form-control' placeholder='Email'/>
+            <div className='signup__form__group'>
+                 <input type="text" className='signup__form__control' placeholder='Email'/>
             </div>
-            <div className='form-group'>
-                 <input type="text" className='form-control' placeholder='Password'/>
+            <div className='signup__form__group'>
+                 <input type="text" className='signup__form__control' placeholder='Password'/>
             </div>
-            <div className='form-group'>
-                 <input type="text" className='form-control' placeholder='Confirm Password'/>
+            <div className='signup__form__group'>
+                 <input type="text" className='signup__form__control' placeholder='Confirm Password'/>
             </div>
 
             {/* Sign up button */}
-            <button type="submit" className="form-btn">SIGN UP</button>
+            <button type="submit" className="signup__form__submit-btn">SIGN UP</button>
                 <p>
                     Already Have An Account?
                 </p>
