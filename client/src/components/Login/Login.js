062498d8942f278d937f330eb4618436c9a02a31
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-15 15:34:47 +0530

    Created login form

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import "./Login.scss"
+import React from "react";
+import "./Login.scss";
+
+//images
+import Logo from "../../assets/images/logos/Crestera-Logo.png";
+import Google from "../../assets/images/Icons/google.png";
 
 function Login() {
   return (
-    <div className='logo'>Login
-    <div className='logo__container'></div>
+    <div className="logo">
+      <div className="logo__container">
+        <form className="login__form">
+          <img src={Logo} alt="cresetra__logo" />
+          <h1>LOGIN</h1>
+
+          <div className="login__form__group">
+            <input
+              type="text"
+              className="login__from__control"
+              placeholder="Email"
+            />
+          </div>
+          <div className="login__form__group">
+            <input
+              type="text"
+              className="login__from__control"
+              placeholder="Password"
+            />
+          </div>
+
+          <button type="submit" className="login__form__btn">
+            LOGIN
+          </button>
+          <hr />
+
+          <button type="button" className="signup__form__google-btn">
+            <img
+              src={Google}
+              alt="google-logo"
+              width={30}
+              className="signup__form__google"
+            />
+            <span>Sign up with google</span>
+          </button>
+          <p>New User?</p>
+          <a href="#">Signup</a>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
