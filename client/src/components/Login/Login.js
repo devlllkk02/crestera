3922fb78578245b23041774d73039c5483f37999
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-15 14:30:42 +0530

    Created login component

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Login.scss"
+
+function Login() {
+  return (
+    <div className='logo'>Login
+    <div className='logo__container'></div>
+    </div>
+  )
+}
+
+export default Login
