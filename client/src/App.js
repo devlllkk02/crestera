d665b115e80ed33a9fd47059f8d9ac458573e1bd
vidithw20 <vidithw99@gmail.com>
2022-03-16 12:05:37 +0530

    init usercircles

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // Components
 import Navbar from "./components/Navbar/Navbar";
 import DashHome from "./components/Dashboard/DashHome/DashHome";
-
+import Usercirclescreate from './components/Usercircles/Usercirclescreate/Usercirclescreate';
 
 //? note add rotes
 const App = () => {
@@ -12,10 +12,10 @@ const App = () => {
       <Navbar />
       <Router>
         <Routes>
-          <Route exact path="/" element={<DashHome/>} />
+          <Route exact path="/" element={<DashHome />} />
+          <Route exact path="/usercircles" element={<Usercirclescreate />} />
         </Routes>
       </Router>
-
     </div>
   );
 };
---
+++ client/src/components/Usercircles/Usercirclescreate/Usercirclescreate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '@fontsource/roboto';
+import './Usercirclescreate.scss';
+
+function Usercirclescreate() {
+  return (
+    <div className="usercircles">
+      <div className="usercircles_header">
+        <p>USER CIRCLES</p>
+      </div>
+      <div className="usercircles_header_container"></div>
+    </div>
+  );
+}
+
+export default Usercirclescreate;
