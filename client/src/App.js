ac632b8efb63ff76c3e2f68acd88e310eb11bc1b
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-16 14:09:57 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./components/Login/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from './components/Signup/Signup';
+
 // import Navbar from "./components/Navbar/Navbar";
 // import DashHome from "./components/Dashboard/DashHome/DashHome";
 
@@ -9,7 +10,6 @@ const App = () => {
   return (
     <div className="app">
     
-      {/* <Signup/> */}
       {/* <Navbar />
       <DashHome /> */}
       <Router>
