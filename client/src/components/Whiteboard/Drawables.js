a4c3c5865094c85e36b78bfbe587552af202712c
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-06-02 20:20:20 +0530

    Created class to draw Arrow

--- client/src/components/Whiteboard/Drawables.js
+++ client/src/components/Whiteboard/Drawables.js
@@ -1,9 +1,28 @@
 import React, {Component} from "react";
-
+import {Arrow} from 'react-konva';
 class Drawable{
     constructor(startx,starty){
         this.startx = startx;
         this.starty = starty;
+    }
+}
+
+//Arrow drawable class
+class ArrowDrawable extends Drawable{
+    constructor(startx,starty){
+        super(startx,starty);
+        this.x = startx;
+        this.y = starty;
+    }
+    //register movements of the mouse points
+    recordMovement(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    render(){
+        const points = [this.startx, this.starty ,this.x, this.y];
+        return <Arrow points={points} fill= "black" stroke="black" />
     }
 }
 class Drawables extends Component{
