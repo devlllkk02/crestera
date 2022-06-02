6cdeec6f685ac4271b2b8856e72a441b0dea2f7c
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-06-02 19:57:31 +0530

    Installed react-konva

--- client/src/components/Whiteboard/Drawables.js
+++ client/src/components/Whiteboard/Drawables.js
@@ -1,5 +1,11 @@
 import React, {Component} from "react";
 
+class Drawable{
+    constructor(startx,starty){
+        this.startx = startx;
+        this.starty = starty;
+    }
+}
 class Drawables extends Component{
 render(){
     return;
