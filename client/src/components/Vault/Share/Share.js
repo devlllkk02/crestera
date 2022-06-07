45d05a04059d5c65a316259a3d387d9310abb58f
vidithw20 <vidithw99@gmail.com>
2022-06-08 02:20:28 +0530

    added vault rename & detail popup,updated vault share

--- client/src/components/Vault/Share/Share.js
+++ client/src/components/Vault/Share/Share.js
@@ -6,6 +6,7 @@ import { useState } from 'react';
 //Images
 import fileicon from '../../../assets/images/Vault icons/FileIcon.png';
 import foldericon from '../../../assets/images/Vault icons/FolderIcon.png';
+import profilePic from '../../../assets/images/other/profilePicture.jpg'
 
 //Font Awesome
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
@@ -62,6 +63,132 @@ function Share() {
           </div>
           <button className="vaultshare_link_button">
             <span>COPY LINK</span>
+          </button>
+        </div>
+      </div>
+      <div className="vaultshare_link_group">
+        <div className="vaultshare_link_group_header">
+          <p>SHARE WITH PEOPLES & GROUPS</p>
+        </div>
+        <button className="vaultshare_link_group_button">
+          <span>INVITE PEOPLES & GROUPS</span>
+        </button>
+      </div>
+      <div className="vaultshare_list_header">
+        <div className="vaultshare_list_header_title1">
+          <p>Username</p>
+        </div>
+        <div className="vaultshare_list_header_title2">
+          <p>Permission</p>
+        </div>
+      </div>
+      <div className="vaultshare_list_items">
+      <div className="vaultshare_list_item__Icon">
+        <img src={profilePic} alt="" />
+      </div>
+      <div className="vaultshare_list_item__Name">
+        <p>Naveen Liyanage</p>
+      </div>
+      <div className="vaultshare__list_item_accessbox">
+            <select name="" id="">
+              <option value="">Viewer</option>
+              <option value="">Editor</option>
+            </select>
+            <div className="vaultshare__list_item_accessbox__icon">
+              <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+          </div>
+          <div className="vaultshare__list_item__remove__container">
+          <button className="vaultshare__list_item__remove_button">
+            <span>REMOVE</span>
+          </button>
+        </div>
+      </div>
+      <div className="vaultshare_list_items">
+      <div className="vaultshare_list_item__Icon">
+        <img src={profilePic} alt="" />
+      </div>
+      <div className="vaultshare_list_item__Name">
+        <p>Naveen Liyanage</p>
+      </div>
+      <div className="vaultshare__list_item_accessbox">
+            <select name="" id="">
+              <option value="">Viewer</option>
+              <option value="">Editor</option>
+            </select>
+            <div className="vaultshare__list_item_accessbox__icon">
+              <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+          </div>
+          <div className="vaultshare__list_item__remove__container">
+          <button className="vaultshare__list_item__remove_button">
+            <span>REMOVE</span>
+          </button>
+        </div>
+      </div>
+      <div className="vaultshare_list_items">
+      <div className="vaultshare_list_item__Icon">
+        <img src={profilePic} alt="" />
+      </div>
+      <div className="vaultshare_list_item__Name">
+        <p>Naveen Liyanage</p>
+      </div>
+      <div className="vaultshare__list_item_accessbox">
+            <select name="" id="">
+              <option value="">Viewer</option>
+              <option value="">Editor</option>
+            </select>
+            <div className="vaultshare__list_item_accessbox__icon">
+              <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+          </div>
+          <div className="vaultshare__list_item__remove__container">
+          <button className="vaultshare__list_item__remove_button">
+            <span>REMOVE</span>
+          </button>
+        </div>
+      </div>
+      <div className="vaultshare_list_items">
+      <div className="vaultshare_list_item__Icon">
+        <img src={profilePic} alt="" />
+      </div>
+      <div className="vaultshare_list_item__Name">
+        <p>Naveen Liyanage</p>
+      </div>
+      <div className="vaultshare__list_item_accessbox">
+            <select name="" id="">
+              <option value="">Viewer</option>
+              <option value="">Editor</option>
+            </select>
+            <div className="vaultshare__list_item_accessbox__icon">
+              <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+          </div>
+          <div className="vaultshare__list_item__remove__container">
+          <button className="vaultshare__list_item__remove_button">
+            <span>REMOVE</span>
+          </button>
+        </div>
+      </div>
+      <div className="vaultshare_list_items">
+      <div className="vaultshare_list_item__Icon">
+        <img src={profilePic} alt="" />
+      </div>
+      <div className="vaultshare_list_item__Name">
+        <p>Naveen Liyanage</p>
+      </div>
+      <div className="vaultshare__list_item_accessbox">
+            <select name="" id="">
+              <option value="">Viewer</option>
+              <option value="">Editor</option>
+            </select>
+            <div className="vaultshare__list_item_accessbox__icon">
+              <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+          </div>
+          <div className="vaultshare__list_item__remove__container">
+          <button className="vaultshare__list_item__remove_button">
+            <span>REMOVE</span>
           </button>
         </div>
       </div>
