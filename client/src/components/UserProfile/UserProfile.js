26ec0ab66bc1e17cf864bdd70543b89d429375e9
Heshani99 <heshaniwick99@gmail.com>
2022-06-09 04:10:57 +0530

    changes in user profile page

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -1,1 +1,71 @@
-
+//------ User Profile Page ------
+import React from "react";
+import "./UserProfile.scss";
+ 
+//images
+import profilePic from "../../assets/images/other/profilePicture.jpg";
+ 
+function UserProfile() {
+  return (
+    <div className="profile__page">
+      <div className="profile__container">
+        <div className="profile__pic">
+          <img src={profilePic} />
+        </div>
+        <div className="profile__heading">
+          <p>PUBLIC PROFILE</p>
+          <hr />
+        </div>
+        <div className="profile__details">
+          <div className="profile__name">
+            <div className="container">
+              <h3>FIRST NAME</h3>
+              <p2>Janice</p2>
+              <hr/>
+            </div>
+            <div className="container">
+              <h3>LAST NAME</h3>
+              <p2>Brownwell</p2>
+              <hr/>
+            </div>
+          </div>
+          <div className="container">
+              <h3>EMAIL</h3>
+              <p2>janicebrownwell@gmail.com</p2>
+              <hr/>
+            </div>
+            <div className="bio">
+          <div className="container">
+            <h3>BIO</h3>
+            <p2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p2>
+            <hr/>
+          </div>
+            </div>
+          <div className="container">
+            <h3>DOB</h3>
+            <p2>12/07/1997</p2>
+            <hr/>
+          </div>
+          <div className="container">
+            <h3>GENDER</h3>
+            <p2>Female</p2>
+            <hr/>
+          </div>
+ 
+        </div>
+        <div className="buttons">
+          <button className="edit">EDIT</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default UserProfile;
