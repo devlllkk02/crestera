647570fcd6cf06f11d8ec1572c243f615b3f89b9
Heshani99 <heshaniwick99@gmail.com>
2022-06-19 12:51:18 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import DashBoardPage from "./pages/DashBoardPage/DashBoardPage";
 import DashNotePage from "./pages/DashNotePage/DashNotePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignupPage from "./pages/SignupPage/SignupPage";
-import UserEditPage from "./pages/UserEditPage/UserEditPage";
+import UserEditPage from "./components/OtherUserEdit/otherUserEdit";
 import UserProfilePage from "./pages/UserProfilePage/UserProfilePage";
 
 // import UserCirclePage from './pages/UserCirclePage/UserCirclePage';
