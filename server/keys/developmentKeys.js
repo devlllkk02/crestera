54b563a1224fab965ce210574c408639708d8ff2
Naveen Liyanage <devlllkk02@gmail.com>
2022-06-19 18:36:27 +0530

    added development keys to github

---
+++ server/keys/developmentKeys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  DATABASE_CONNECTION:
+    "mongodb+srv://crestera:nVdZF6aweFZaph4C@cluster0.haikw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  JWT_SECRET: "GA4pGAk4kyyLGSeTbiwH",
+  SOCKET_PORT:"",
+  CLOUDINARY_API_KEY: "393459479285331",
+  CLOUDINARY_API_SECRET: "OJyYLf-ineOg7-j3NBKKaWgcd_U",
+  CLOUDINARY_API_CLOUD: "dlaimiqfi",
+};
